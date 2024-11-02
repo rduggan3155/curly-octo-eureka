b49029b7de892e3ae767b40c0394c350393d841d
Rebecca Duggan <rduggan3155@gmail.com>
2024-11-02 14:23:12 -0400

    week-11-POC

--- week-11-cy-automation/cypress.config.js
+++ week-11-cy-automation/cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.amazon.jobs/en/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
--- week-11-cy-automation/cypress/e2e/setUp.cy.js
+++ week-11-cy-automation/cypress/e2e/setUp.cy.js
@@ -1,3 +1,7 @@
+beforeEach(() => {
+  cy.visit("");
+});
+
 describe("My First Test", () => {
   it("Does not do much!", () => {
     expect(true).to.equal(true);
